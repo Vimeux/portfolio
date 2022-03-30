37083615e3bdbf0c341a505699ed9fcb17a77ab5
BenjaminZoan <benjamin@zoan.fr>
2022-03-30 09:54:38 +0200

    add locomotive

--- src/common/components/Layout/Layout.js
+++ src/common/components/Layout/Layout.js
@@ -4,7 +4,7 @@ export default function Layout ({ children }) {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <main data-scroll-container>{children}</main>
     </>
   )
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,36 @@
+import { useEffect } from 'react'
 import Layout from '../common/components/Layout/Layout'
 import '../styles/globals.scss'
 
 function MyApp ({ Component, pageProps }) {
+  // Manage locomotive
+  useEffect(() => {
+    let scroll
+    import('locomotive-scroll').then((locomotiveModule) => {
+      scroll = new locomotiveModule.default({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true,
+        smoothMobile: true,
+        resetNativeScroll: true,
+        lerp: 0.1,
+        multiplier: 1,
+        offset: ['30%', 100]
+      })
+    })
+
+    // `useEffect`'s cleanup phase
+    return () => scroll.destroy()
+  })
+
+  // Each time the component is modified, the page size is recalculated according to the size of the component.
+  useEffect(() => window.dispatchEvent(new Event('resize')), [Component])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   )
 }
 
